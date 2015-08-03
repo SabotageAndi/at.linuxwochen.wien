bfb071e9445a844f46677bce49d3dedf0a41591d
Andreas Willich <sabotageandi@gmail.com>
2015-08-03 22:43:43 +0200

    add missing files

---
+++ ffrab-mobile.common.ui/Menu.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ffrab.mobile.common.ui
+{
+    public partial class Menu : ContentPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
