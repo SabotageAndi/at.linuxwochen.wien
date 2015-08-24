ed19691e516b5819c64e1583e647ec0f473b0a6c
Andreas Willich <sabotageandi@gmail.com>
2015-08-24 19:36:10 +0200

    delete Conference Data tests

--- ffrab.test/database.fs
+++ ffrab.test/database.fs
@@ -26,12 +26,54 @@
         let db = init()
         Conferences.Database.reCreateDatabase()
     
-    [<Fact>]   
+    [<Fact>]
     let ``Entries removed on conference clean``() =
         let db = initWithRecreate()
 
         let entry = new Entry(ConferenceId = 1)
         db.Insert entry |> should equal 1
+        
+        db.Table<Entry>().Count() |> should equal 1
 
         Conferences.Database.deleteConference conf
 
+        db.Table<Entry>().Count() |> should equal 0
+
+    [<Fact>]
+    let ``Rooms removed on conference clean``() =
+        let db = initWithRecreate()
+
+        let room = new Room(ConferenceId = 1)
+        db.Insert room |> should equal 1
+
+        db.Table<Room>().Count() |> should equal 1
+
+        Conferences.Database.deleteConference conf
+
+        db.Table<Room>().Count() |> should equal 0
+
+    [<Fact>]
+    let ``Conference Day removed on conference clean``() =
+        let db = initWithRecreate()
+
+        let conferenceDay = new ConferenceDay(ConferenceId = 1)
+        db.Insert conferenceDay |> should equal 1
+
+        db.Table<ConferenceDay>().Count() |> should equal 1
+
+        Conferences.Database.deleteConference conf
+
+        db.Table<ConferenceDay>().Count() |> should equal 0
+
+    [<Fact>]
+    let ``ConferenceData removed on conference clean``() =
+        let db = initWithRecreate()
+
+        let conferenceData = new ConferenceData(ConferenceId = 1)
+        db.Insert conferenceData |> should equal 1
+
+        db.Table<ConferenceData>().Count() |> should equal 1
+
+        Conferences.Database.deleteConference conf
+
+        db.Table<ConferenceData>().Count() |> should equal 0
